0d912fa51c5f295587a4171dc14508ac06e54b44
Zane Reid <zanecreid@hotmail.com>
2023-03-13 11:39:15 +0000

    Top Bar created

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
-// color design tokens
+// color design tokens export
 export const tokens = (mode) => ({
   ...(mode === "dark"
     ? {
-        gray: {
+        grey: {
           100: "#e0e0e0",
           200: "#c2c2c2",
           300: "#a3a3a3",
@@ -20,7 +20,7 @@ export const tokens = (mode) => ({
           100: "#d0d1d5",
           200: "#a1a4ab",
           300: "#727681",
-          400: "#434957",
+          400: "#1F2A40",
           500: "#141b2d",
           600: "#101624",
           700: "#0c101b",
@@ -62,7 +62,7 @@ export const tokens = (mode) => ({
         },
       }
     : {
-        gray: {
+        grey: {
           100: "#141414",
           200: "#292929",
           300: "#3d3d3d",
@@ -77,9 +77,9 @@ export const tokens = (mode) => ({
           100: "#040509",
           200: "#080b12",
           300: "#0c101b",
-          400: "#f2f0f0",
+          400: "#f2f0f0", // manually changed
           500: "#141b2d",
-          600: "#434957",
+          600: "#1F2A40",
           700: "#727681",
           800: "#a1a4ab",
           900: "#d0d1d5",
@@ -123,12 +123,12 @@ export const tokens = (mode) => ({
 // mui theme settings
 export const themeSettings = (mode) => {
   const colors = tokens(mode);
-
   return {
     palette: {
       mode: mode,
       ...(mode === "dark"
         ? {
+            // palette values for dark mode
             primary: {
               main: colors.primary[500],
             },
@@ -136,7 +136,7 @@ export const themeSettings = (mode) => {
               main: colors.greenAccent[500],
             },
             neutral: {
-              dark: colors.gray[700],
+              dark: colors.grey[700],
               main: colors.grey[500],
               light: colors.grey[100],
             },
@@ -145,6 +145,7 @@ export const themeSettings = (mode) => {
             },
           }
         : {
+            // palette values for light mode
             primary: {
               main: colors.primary[100],
             },
@@ -152,7 +153,7 @@ export const themeSettings = (mode) => {
               main: colors.greenAccent[500],
             },
             neutral: {
-              dark: colors.gray[700],
+              dark: colors.grey[700],
               main: colors.grey[500],
               light: colors.grey[100],
             },
@@ -164,10 +165,6 @@ export const themeSettings = (mode) => {
     typography: {
       fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
       fontSize: 12,
-      h1: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-        fontSize: 40,
-      },
       h1: {
         fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 40,
@@ -196,7 +193,7 @@ export const themeSettings = (mode) => {
   };
 };
 
-// content for color mode
+// context for color mode
 export const ColorModeContext = createContext({
   toggleColorMode: () => {},
 });
@@ -211,7 +208,7 @@ export const useMode = () => {
     }),
     []
   );
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return [theme, colorMode];
 };
