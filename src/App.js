42934f436757bea48be2ec354dde0c2d1d7ad64a
Zane Reid <zanecreid@hotmail.com>
2023-03-14 06:46:39 +0000

    SideBar created

--- src/App.js
+++ src/App.js
@@ -23,19 +23,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideBar />
           <main className="content">
             <Topbar />
-            <Route path="/" element={<Dashboard />} />
-            {/* <Route path="/team" element={<Team />} /> */}
-            {/* <Route path="/contacts" element={<Contacts />} /> */}
-            {/* <Route path="/invoices" element={<Invoices />} /> */}
-            {/* <Route path="/form" element={<Form />} /> */}
-            {/* <Route path="/bar" element={<Bar />} /> */}
-            {/* <Route path="/pie" element={<Pie />} /> */}
-            {/* <Route path="/line" element={<Line />} /> */}
-            {/* <Route path="/faq" element={<FAQ />} /> */}
-            {/* <Route path="/geography" element={<Geography />} /> */}
-            {/* <Route path="/calendar" element={<Calendar />} /> */}
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
