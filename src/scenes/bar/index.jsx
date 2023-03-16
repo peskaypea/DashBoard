2d086be12e64d7f71c9e86efdebbf0eccc1a0ee3
Zane Reid <zanecreid@hotmail.com>
2023-03-16 14:04:47 +0000

    pie chart completed

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import BarChart from "../../components/BarChart";
 
-const Bar = ({}) => {
+const Bar = () => {
   return (
     <Box m="20px">
       <Header title="Bar Chart" subtitle="Simple Bar Chart" />
